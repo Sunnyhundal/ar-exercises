b6148875658f6538700ca23d50bb59990901e8d7
Sunny Hundal <amandip.h@gmail.com>
2023-10-27 15:55:29 -0700

    EX 6 created new employees and defined relationships between stores and employees. EX7 add validation fo stores and employees

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,11 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+puts "Please enter a store name:"
+get_store_name = gets.chomp
+
+# create a new store
+new_store = Store.create(name: get_store_name)
+
+# error messages 
+puts new_store.errors.full_messages
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,5 @@
 class Store < ActiveRecord::Base
-
+has_many :employees
+validates_length_of :name, minimum: 3, too_short: "must be at least 3 characters long"
+validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
