9b0d36563d1d9a9c7b3ef49211986d82cc73ac2b
Sunny Hundal <amandip.h@gmail.com>
2023-10-26 21:50:45 -0700

    Calculations for stores (sum, avg, and use where)

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,11 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+puts "Total revenue for all stores: #{total_revenue}"
+
+average_revenue = Store.sum(:annual_revenue)/Store.count
+puts "Average revenue for all stores: #{average_revenue}"
+
+over_million = Store.where("annual_revenue >= 1000000").count
+puts "Number of stores with annual sales of $1M or more: #{over_million}"
