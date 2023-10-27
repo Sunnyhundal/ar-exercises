02a0d297d571c6322170922b4de5d6ad23e3cc05
Sunny Hundal <amandip.h@gmail.com>
2023-10-26 19:27:37 -0700

    Create method to find by id and store value in instance variables

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,14 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
+def find_by_id(id)
+  Store.find(id)
+end
+
+@store1 = find_by_id(1)
+
+@store2 = find_by_id(2)
+
+@store1.name = "New Burnaby"
+puts @store1.name
 
